700e9d6507156a041821570f9abd33cfa8884e3f
Jesse Aldridge <JesseAldridge@gmail.com>
2011-11-17 17:19:08 -0600

    up to 58

--- 028_spiral.py
+++ 028_spiral.py
@@ -5,12 +5,16 @@
 19  6  1  2 11
 18  5  4  3 12
 17 16 15 14 13
+
+What is the sum of the numbers on the diagonals of a 1001 by 1001 spiral?
 '''
+
 grid_width = 1001
 curr_num = 1
 sum_ = 1
-for cycle in range(2, grid_width, 2):
+
+for dist in range(2, grid_width, 2):
   for i in range(4):
-    curr_num += cycle
+    curr_num += dist
     sum_ += curr_num
 print sum_
