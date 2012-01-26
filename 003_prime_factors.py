9672b65d96ab0873b4b87b3f58861519f5db2e96
Jesse Aldridge <JesseAldridge@gmail.com>
2012-01-26 09:27:35 -0600

    60 and 61

--- 003_prime_factors.py
+++ 003_prime_factors.py
@@ -1,9 +1,14 @@
 
+import operator
+
 def prime_factors(n):
+  ' Recursively divide n by the smallest factor that divides it evenly. '
   if n == 1:  return [1]
   for i in range(2, int(n ** .5) + 1):
     if n % i == 0:
+      # If nothing less than i divides n evenly, i must be prime.
       return [i] + prime_factors(n / i)
   return [n, 1]
-  
+
 print prime_factors(600851475143)
+print reduce(operator.mul, prime_factors(600851475143))
--- 005_divisible.py
+++ 005_divisible.py
@@ -4,7 +4,9 @@ def lcm(*args):
   return args[0] * args[1] /  gcd(args[0], args[1])
 
 def gcd(a, b):
-  while b:  a, b = b, a % b        
+  while b:  a, b = b, a % b
   return a
 
+# Find the smallest number evenly divisible by every number in 1 through 21.
 print lcm(*[i for i in range(1, 21)])
+print 232792560 % 21, 232792560 % 20, 232792560 % 19
